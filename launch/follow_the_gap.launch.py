603aa47806a866c5a87d588a843c5f85c3185c4d
Kälin Tommy Lee (kaey) <kaey@zhaw.ch>
2026-02-14 16:24:16 +0100

    launch config was wrong

--- launch/follow_the_gap.launch.py
+++ launch/follow_the_gap.launch.py
@@ -7,7 +7,7 @@ def generate_launch_description():
     follow_the_gap_node = Node(
         package="follow_the_gap",
         executable="follow_the_gap",
-        name="follow_the_gap_controller",
+        name="follow_the_gap",
         output="screen",
     )
 
